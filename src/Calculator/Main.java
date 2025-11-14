0604a5de6eaad926c6bdcd4b7bb2e2598ca51285
Muhammad-Zain-ul-Hassan <f219466@cfd.nu.edu.pk>
2025-11-14 15:06:47 +0500

    Added multiply function to Calculator

--- src/Calculator/Main.java
+++ src/Calculator/Main.java
@@ -12,10 +12,6 @@ public class Main {
         System.out.println("10 - 5 = " + difference);
         
        
-        System.out.println("\nMore examples:");
-        System.out.println("25 + 15 = " + calc.add(25, 15));
-        System.out.println("100 - 37 = " + calc.subtract(100, 37));
-        System.out.println("0 + 0 = " + calc.add(0, 0));
-        System.out.println("50 - 50 = " + calc.subtract(50, 50));
+    ;
     }
 }
