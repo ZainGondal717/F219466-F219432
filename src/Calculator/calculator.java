8dc9617e55c1f9e25cd6561e1264a215328c4046
Shahmeer Hussain <100699061+shahmeer3456@users.noreply.github.com>
2025-11-20 21:40:57 +0500

    Update calculator.java

--- src/Calculator/calculator.java
+++ src/Calculator/calculator.java
@@ -1,14 +1,1 @@
-package Calculator;
 
-public class calculator {
-    public int add(int a, int b) {
-        return a + b;
-    }
-
-    public int subtract(int a, int b) {
-        return a - b;
-    }
-    public int multiply(int a, int b) {
-        return a * b;
-    }
-}
